f604b3b219cfc1efd8a86228d56624d63de16f21
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-24 16:15:26 -0300

    feat(layout): adding header in Layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,11 @@
+import { Header } from '@/components/Header';
 import { Outlet } from 'react-router-dom';
 
 export function Layout() {
   return (
-    <div className='bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
+    <div className='px-20  pt-6 before:-z-10 bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
+      <Header />
+
       <main>
         <Outlet />
       </main>
