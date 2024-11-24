c6c7f63970d1f7d33741e11eefbe437bc21ba4b2
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-24 13:50:08 -0300

    feat(layout): adding Footer in Layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,13 @@
+import { Footer } from '@/components/Footer';
 import { Outlet } from 'react-router-dom';
 
 export function Layout() {
   return (
-    <div className='bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
+    <div className='relative bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
