2749032869860945db15888e0e28a1f5f3c3c5bb
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-25 06:30:14 -0300

    feat(components): adding padding in xaxis

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from '../ThemeSwitcher';
 
 export function Header() {
   return (
-    <header className='flex justify-end items-center gap-6'>
+    <header className='px-20 flex justify-end items-center gap-6'>
       <ThemeSwitcher theme={'light'} toggleThemeMode={(theme) => {}} />
 
       <Dropdown />
