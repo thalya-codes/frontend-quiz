1f8a0076896b4d4fea27d26930bd42ca0e0e277e
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-25 06:30:36 -0300

    fix(components): removing padding in xaxis(Layout)

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 export function Layout() {
   return (
-    <div className='px-20  pt-6 before:-z-10 bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
+    <div className='pt-6 before:-z-10 bg-light-purple w-screen h-screen before:absolute before:-top-9 before:-left-5 before:content-[url("/assets/blob-top-left.svg")]  bg-no-repeat bg-right-blob-position bg-[url("/assets/blob-bottom-right.svg")]'>
       <Header />
 
       <main>
