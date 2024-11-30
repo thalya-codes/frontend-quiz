082c5f7a25fd554078a720bbbe40557651957298
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-30 20:48:54 -0300

    feat(components): creating size and font variants for the component Typography

---
+++ src/components/Typography/variants.tsx
@@ -0,0 +1,11 @@
+export const typografyFontVariants = {
+  primary: 'font-quicksand text-chocolate',
+  secondary: 'font-nova-slim text-medium-gray tracking-[2px]',
+};
+
+export const typografySizeVariants = {
+  sm: 'text-sm',
+  md: 'text-xl',
+  lg: 'text-2xl',
+  xl: 'text-4xl',
+};
