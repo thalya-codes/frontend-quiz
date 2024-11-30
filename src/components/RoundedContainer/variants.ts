1095151552c89de4b0365dc19c8bcf46088777be
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-30 19:45:53 -0300

    feat(components): creating variants for component RoundedContainer

---
+++ src/components/RoundedContainer/variants.ts
@@ -0,0 +1,7 @@
+export const roundedContainerVariants = {
+    white: 'bg-white',
+    green: 'bg-mint-green',
+    purple: 'bg-strong-purple', 
+    yellow: 'bg-bright-yellow',
+    peach:'bg-peach'
+}
