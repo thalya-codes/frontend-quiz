dce7cb7f5dcb655e859ac21add78adbe208b100e
Thalya Stéffany <thalyacoiimbra@gmail.com>
2024-11-30 23:47:37 -0300

    feat(components): adding variants in the component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,15 @@
-import { ButtonHTMLAttributes } from 'react';
+import { buttonVariants } from './variants';
+import { IButton } from './IButton';
 
 export function Button({
   className = '',
+  variant = 'purple',
   ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
+}: IButton) {
   return (
     <button
       {...props}
-      className={`w-full font-quicksand font-bold transition-all ease-in-out hover:bg-opacity-90 text-white tracking-1 shadow-elevation-2 rounded-lg py-4 uppercase text-center  ${className}`}
+      className={`w-full  font-quicksand font-bold transition-all ease-in-out hover:bg-opacity-90 text-white tracking-1 shadow-elevation-2 rounded-lg py-4 uppercase text-center ${buttonVariants[variant]} ${className}`}
     />
   );
 }
